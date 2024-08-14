1725fb16f861c8415f8b43bf4fd1739bb39ac78b
Zhikai_Wang <zhikaiwang1992@gmail.com>
2024-08-14 15:33:03 +0200

    Update R_learning.R

--- R_learning.R
+++ R_learning.R
@@ -1,16 +1,34 @@
 # test1.R
-x = 1:10
-y = x^2
-z = "hello world"
-d = 'tt'
-print(z)
-print(y)
-print(d)
+
+## function with return value(s)
+fun2=function(arg1, arg2) {
+  if(arg1 > arg2) {
+    return (arg1)
+} else {
+    return (arg2)}
+}
+print(paste("max value is",fun2(100, 20)))
+
+
+## function without return value
+fun1=function(arg1="Hello", arg2) {
+  print(paste(arg1,arg2))
+}
+fun1("Bonjour","World")
+
 
 ## simple plot point figure
 a = c(1,2,3,4,5)
 b = a^2
 plot(a,b)
+
+
+## R list
+names = list("John","Emma","Tonny","Ben")
+for (x in names) {
+  print(x)
+}
+
 
 ## 'for' loops
 for (i in 1:10) {
@@ -18,7 +36,19 @@ print (i)
 }
 
 
-## if else
+## R 'while' for loops
+misfit0   = 100
+threshold =  50
+while (misfit0 > threshold) {
+  print(paste('current misfit is',misfit0))
+  misfit0 = misfit0 * 0.9
+
+  if(misfit0 < 60) {
+    break}
+}
+
+
+##  R 'if else'
 a=1
 b=2
 c=3
@@ -27,19 +57,18 @@ if(a>b) {
   if(a>c) {
     print(paste("max is a=",a)) } else {
     print('max is c=',c) }
-} else if(b>c) { 
+} else if(b>c) {
 print ('max is b=',b)
 } else{
 print (paste('max is c=',c))}
 
-## 'while' for loops
-misfit0   = 100
-threshold =  50
-while (misfit0 > threshold) {
-  print(paste('current misfit is',misfit0))
-  misfit0 = misfit0 * 0.9
-  
-  if(misfit0 < 60) { 
-    break}
-}
+
+## R 'print'
+x = 1:10
+y = x^2
+z = "hello world"
+d = 'tt'
+print(z)
+print(y)
+print(d)
 
